71fe7a0fad0a36f74f9382cf11b6f2cc84e5f3c1
smithbwlhs <smithb@wlwv.k12.or.us>
2024-12-15 22:24:16 -0800

    initial commit

---
+++ linear_calcuations.py
@@ -0,0 +1,17 @@
+"""
+Name:
+Date:
+Description:
+"""
+
+def slope(point1:tuple,point2:tuple)->float:
+    return 0.0
+
+def distance(point1:tuple, point2:tuple)->float:
+    return 0.0
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ tuple_lesson.py
@@ -0,0 +1,37 @@
+"""
+Name:
+Date:
+Lesson: Tuples
+"""
+
+# Tuples are a heterogeneous (mixed types), immutable data structure
+# How to pronounce, from the Python creator:
+"""
+I pronounce tuple too-pull on Mon/Wed/Fri and tub-pull on Tue/Thu/Sat. 
+On Sunday I don't talk about them. :) 
+"""
+# The look like an ordered pair in math
+
+# Make tuple with two items
+
+# Make tuple with one item
+
+
+# You access elements from them using [ ] just like strings
+
+
+
+# Also like strings, you can only concatenate tuples with other tuples
+
+# I heard you like tuples...
+# so you can also put tuples inside your tuples
+# need two [] to access them.
+
+
+########## Exercise ##########
+
+"""
+In the file linear_calculations.py, complete the slope and distance
+function. Each function takes in two tuples, representing two (x,y) points,
+and returns the slope or distance as a float. 
+"""

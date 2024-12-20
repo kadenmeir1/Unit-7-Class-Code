39044f85951ff22516c8765a46561b92e3e2b809
kaden <127469055+kadenmeir1@users.noreply.github.com>
2024-12-20 08:58:18 -0800

    Update linear_calcuations.py

--- linear_calcuations.py
+++ linear_calcuations.py
@@ -3,15 +3,27 @@ Name:
 Date:
 Description:
 """
-
+import math
 def slope(point1:tuple,point2:tuple)->float:
-    return 0.0
-
+    x1 = point1[0]
+    x2 = point2[0]
+    y1 = point1[1]
+    y2 = point2[2]
+    slope = (y2 - y1)/(x2 - x1)
+    return slope
 def distance(point1:tuple, point2:tuple)->float:
-    return 0.0
+    x1 = point1[0]
+    x2 = point2[0]
+    y1 = point1[1]
+    y2 = point2[2]
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2
+    return distance
 
 def main():
-    pass
+    point1 = (3,4)
+    point2 = (-8,12)
+    print(slope(point1, point2))
+    print(distance(point1, point2))
 
 if __name__ == "__main__":
     main()
